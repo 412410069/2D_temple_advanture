3ddc494264c2c067d2fe3a23ff45c88cbb2194fb
412410043 <liang0121@alum.ccu.edu.tw>
2024-05-23 02:21:29 +0800

    Update MonsterInitial.cs

--- Assets/Script/MonsterInitial.cs
+++ Assets/Script/MonsterInitial.cs
@@ -2,32 +2,29 @@ using UnityEngine;
 
 public class MonsterInitial : MonoBehaviour
 {
-    public Game game;
+    public Game Game;
     public GameObject monster1; 
     public int numberOfClones = 5;
-    Vector2 position;
-    Vector2 position_prefab;
+
     void Awake(){
-        game = GameObject.FindGameObjectWithTag("grid").GetComponent<Game>();
+        Game = GameObject.FindGameObjectWithTag("grid").GetComponent<Game>();
     }
 
     public void GenerateMonster1()
     {
-        for (int i = 0; i < numberOfClones; i++)
+        for (int i = 0; i < numberOfClones-1; i++)
         {
-            float x = Random.Range(0, game.width);
-            float y = Random.Range(0, game.height);
+            float x = Random.Range(8, 24);
+            float y = Random.Range(8, 24);
 
-            if(game.state[(int)x, (int)y].type != Cell.Type.Wall && game.state[(int)x, (int)y].type != Cell.Type.Void)
-            position = new Vector2((int)x, (int)y); 
+            Vector2 position = new Vector2((int)x, (int)y); 
             GameObject clone = Instantiate(monster1, position, Quaternion.identity);
         }
         
-        float x_prefab = Random.Range(0, game.width);
-        float y_prefab = Random.Range(0, game.height);
+        float x_prefab = Random.Range(8, 24);
+        float y_prefab = Random.Range(8, 24);
 
-        if(game.state[(int)x_prefab, (int)y_prefab].type != Cell.Type.Wall && game.state[(int)x_prefab, (int)y_prefab].type != Cell.Type.Void)
-        position_prefab = new Vector2((int)x_prefab, (int)y_prefab);
+        Vector2 position_prefab = new Vector2((int)x_prefab, (int)y_prefab);
 
         monster1.transform.position=position_prefab;
     }
