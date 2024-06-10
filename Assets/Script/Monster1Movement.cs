10909b35789ceccc268c03043eabcb8c679c86ce
412410043 <liang0121@alum.ccu.edu.tw>
2024-06-10 22:27:09 +0800

    修正怪物追蹤
    怪物追蹤時可能從出口跑出地圖外

--- Assets/Script/Monster1Movement.cs
+++ Assets/Script/Monster1Movement.cs
@@ -92,11 +92,11 @@ public class Monster1Movement : MonoBehaviour
 
         for(int i=0;i<2;++i){
             if(direction == 0){
-                if(player_x > x && (game.state[x + 1, y].type != Cell.Type.Wall) && !(game.state[x + 1, y].revealed && game.state[x + 1, y].type == Cell.Type.Mine)){
+                if(player_x > x && (game.state[x + 1, y].type != Cell.Type.Wall) && (game.state[x + 1, y].type != Cell.Type.Exit) && !(game.state[x + 1, y].revealed && game.state[x + 1, y].type == Cell.Type.Mine)){
                     position += new Vector3(1, 0, 0);
                 }
 
-                else if(player_x < x && (game.state[x - 1, y].type != Cell.Type.Wall) && !(game.state[x - 1, y].revealed && game.state[x - 1, y].type == Cell.Type.Mine)){
+                else if(player_x < x && (game.state[x - 1, y].type != Cell.Type.Wall) && (game.state[x - 1, y].type != Cell.Type.Exit) && !(game.state[x - 1, y].revealed && game.state[x - 1, y].type == Cell.Type.Mine)){
                         position += new Vector3(-1, 0, 0);
                 }
                     
@@ -107,11 +107,11 @@ public class Monster1Movement : MonoBehaviour
             }
 
             else if(direction == 1){
-                if(player_y > y && (game.state[x, y + 1].type != Cell.Type.Wall) && !(game.state[x, y + 1].revealed && game.state[x, y + 1].type == Cell.Type.Mine)){
+                if(player_y > y && (game.state[x, y + 1].type != Cell.Type.Wall) && (game.state[x, y + 1].type != Cell.Type.Exit) && !(game.state[x, y + 1].revealed && game.state[x, y + 1].type == Cell.Type.Mine)){
                     position += new Vector3(0, 1, 0);
                 }
 
-                else if(player_y < y && (game.state[x, y - 1].type != Cell.Type.Wall) && !(game.state[x, y - 1].revealed && game.state[x, y - 1].type == Cell.Type.Mine)){
+                else if(player_y < y && (game.state[x, y - 1].type != Cell.Type.Wall) && (game.state[x, y - 1].type != Cell.Type.Exit) && !(game.state[x, y - 1].revealed && game.state[x, y - 1].type == Cell.Type.Mine)){
                         position += new Vector3(0, -1, 0);
                 }
 
